2a99f3e2287bc74113f363e2a226d47c09dc24de
pepelepoisson <pepelepoisson@gmail.com>
2018-10-27 22:33:58 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,136 @@
+/*------------------------------------------------------------------------------
+  07/01/2018
+  Author: Makerbro
+  Platforms: ESP8266
+  Language: C++/Arduino
+  File: webserver_html.ino
+  ------------------------------------------------------------------------------
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+
+ESP8266WebServer server;
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+uint8_t pin_led = 2;
+char* ssid = "********";
+char* password = "********";
+
+#define echoPin D7 // Echo Pin
+#define trigPin D6 // Trigger Pin
+long duration, distance; // Duration used to calculate distance
+
+char webpage[] PROGMEM = R"=====(
+<html>
+<head>
+  <script>
+    var Socket;
+    function init() {
+      Socket = new WebSocket('ws://' + window.location.hostname + ':81/');
+      Socket.onmessage = function(event){
+        document.getElementById("rxConsole").value += event.data;
+      }
+    }
+    function sendText(){
+      Socket.send(document.getElementById("txBar").value);
+      document.getElementById("txBar").value = "";
+    }
+    function sendBrightness(){
+      Socket.send("#"+document.getElementById("brightness").value);
+    }
+  </script>
+</head>
+<body onload="javascript:init()">
+  <div>
+    <textarea id="rxConsole"></textarea>
+  </div>
+  <hr/>
+  <div>
+    <input type="text" id="txBar" onkeydown="if(event.keyCode == 13) sendText();" />
+  </div>
+  <hr/>
+  <div>
+    <input type="range" min="0" max="1023" value="512" id="brightness" oninput="sendBrightness()" />
+  </div>
+</body>
+</html>
+)=====";
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
+  if(type == WStype_TEXT){
+    if(payload[0] == '#'){
+      uint16_t brightness = (uint16_t) strtol((const char *) &payload[1], NULL, 10);
+      brightness = 1024 - brightness;
+      analogWrite(pin_led, brightness);
+      Serial.print("brightness= ");
+      Serial.println(brightness);
+    }
+
+    else{
+      for(int i = 0; i < length; i++)
+        Serial.print((char) payload[i]);
+      Serial.println();
+    }
+  }
+
+}
+void GetDistance()
+{
+/* The following trigPin/echoPin cycle is used to determine the
+distance of the nearest object by bouncing soundwaves off of it. */
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+//Calculate the distance (in cm) based on the speed of sound.
+distance = duration/58.2;
+//Serial.println(distance);
+//Delay 50ms before next reading.
+delay(50);
+}
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(pin_led, OUTPUT);
+  WiFi.begin(ssid,password);
+  Serial.begin(115200);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",[](){
+    server.send_P(200, "text/html", webpage);
+  });
+  server.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+}
+
+void loop()
+{
+  GetDistance();
+  webSocket.loop();
+  server.handleClient();
+  //Serial.println(distance);
+  if(distance<10){
+    char c[] = "y";
+    webSocket.broadcastTXT(c, sizeof(c));
+  }
+
+  if(Serial.available() > 0){
+    char c[] = {(char)Serial.read()};
+    webSocket.broadcastTXT(c, sizeof(c));
+  }
+}
